70636cf584ca4bd441f8395953866d673b045dd8
Abbas Raza <abbas.raxa.25@gmail.com>
2026-04-16 20:25:01 +0500

    Update app.json with bundleIdentifier for iOS and refactor layout code for consistency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,17 @@
-import { Stack } from "expo-router";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Toaster } from "sonner-native";
+import {Stack} from "expo-router";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import {Toaster} from "sonner-native";
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={{ flex: 1 }} edges={["top", "bottom"]}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(app)" />
-        </Stack>
-        <Toaster />
-      </SafeAreaView>
+    <GestureHandlerRootView style={{flex: 1}}>
+      {/* <SafeAreaView style={{flex: 1}} edges={["top", "bottom"]}> */}
+      <Stack screenOptions={{headerShown: false}}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(app)" />
+      </Stack>
+      <Toaster />
+      {/* </SafeAreaView> */}
     </GestureHandlerRootView>
   );
 }
